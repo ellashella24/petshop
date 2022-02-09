2197c52509a14fe5bc718b4e4c8b3c6d5c7d6da0
Niendhitta Tamia Lassela <ella.shella24@gmail.com>
2022-02-09 12:06:43 +0700

    add pet feature v1

--- delivery/route/route.go
+++ delivery/route/route.go
@@ -1,1 +1,24 @@
 package route
+
+import (
+	"petshop/constant"
+	"petshop/delivery/controller/pet"
+	mw "petshop/delivery/middleware"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func RegisterPath(e *echo.Echo, petCtrl *pet.PetController) {
+	eAuth := e.Group("")
+	eAuth.Use(middleware.JWT([]byte(constant.SecretKey)))
+	eAuthAdmin := eAuth.Group("")
+	eAuthAdmin.Use(mw.IsAdmin)
+
+	// Pet Route Path
+	eAuth.POST("/user/pet", petCtrl.CreatePet())
+	eAuth.GET("/user/pets", petCtrl.GetAllPetByUser())
+	eAuth.GET("/user/pet/profile/:id", petCtrl.GetPetProfile())
+	eAuth.PUT("/user/pet/profile/:id", petCtrl.UpdatePetProfile())
+	eAuth.DELETE("/user/pet/:id", petCtrl.DeletePet())
+}
--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"fmt"
 	"petshop/config"
+	petCtrl "petshop/delivery/controller/pet"
 	"petshop/delivery/middleware"
+	"petshop/delivery/route"
+	petRepo "petshop/repository/pet"
 	"petshop/util"
 
 	"github.com/go-playground/validator/v10"
@@ -24,6 +27,13 @@ func main() {
 	e := echo.New()
 
 	middleware.LogMiddleware(e)
+
+	e.Validator = &CustomValidator{validator: validator.New()}
+
+	petRepository := petRepo.NewPetRepository(db)
+	petController := petCtrl.NewPetController(petRepository)
+
+	route.RegisterPath(e, petController)
 
 	fmt.Println(db)
 
