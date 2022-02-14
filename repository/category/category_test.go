e0a798838de65dc88e5b22194d8869505fada177
naufalaammar <hibatullah.nfl@gmail.com>
2022-02-14 16:34:13 +0700

    update unit test category

--- repository/category/category_test.go
+++ repository/category/category_test.go
@@ -1,6 +1,7 @@
 package category
 
 import (
+	"gorm.io/gorm"
 	"petshop/config"
 	"petshop/entity"
 	"petshop/util"
@@ -26,19 +27,23 @@ func TestCreateCategory(t *testing.T) {
 	newCategory := entity.Category{}
 	newCategory.Name = "category2"
 
-	t.Run("1. Success create category", func(t *testing.T) {
-		res, err := categoryRepo.CreateCategory(newCategory)
+	t.Run(
+		"1. Success create category", func(t *testing.T) {
+			res, err := categoryRepo.CreateCategory(newCategory)
 
-		assert.Nil(t, err)
-		assert.Equal(t, "category2", res.Name)
-	})
+			assert.Nil(t, err)
+			assert.Equal(t, "category2", res.Name)
+		},
+	)
 
-	t.Run("2. Error create category - Duplicate name", func(t *testing.T) {
-		res, err := categoryRepo.CreateCategory(newCategory)
+	t.Run(
+		"2. Error create category - Duplicate name", func(t *testing.T) {
+			res, err := categoryRepo.CreateCategory(newCategory)
 
-		assert.NotNil(t, err)
-		assert.Equal(t, 0, int(res.ID))
-	})
+			assert.NotNil(t, err)
+			assert.Equal(t, 0, int(res.ID))
+		},
+	)
 }
 
 func TestGetAllCategory(t *testing.T) {
@@ -47,111 +52,111 @@ func TestGetAllCategory(t *testing.T) {
 
 	categoryRepo := NewCategoryRepository(db)
 
-	newCategory := entity.Category{}
-	newCategory.Name = "category2"
+	t.Run(
+		"1. Success get category", func(t *testing.T) {
+			res, err := categoryRepo.GetAllCategory()
 
-	db.Create(&newCategory)
+			assert.Nil(t, err)
+			assert.Equal(t, "category2", res[0].Name)
+		},
+	)
 
-	t.Run("1. Success get category", func(t *testing.T) {
-		res, err := categoryRepo.GetAllCategory()
+	t.Run(
+		"2. Error get category", func(t *testing.T) {
+			category1 := entity.Category{}
+			db.Where("id = ?", 1).Delete(&category1)
+			res, err := categoryRepo.GetAllCategory()
 
-		assert.Nil(t, err)
-		assert.Equal(t, "category2", res[1].Name)
-	})
-
-	t.Run("2. Error get category", func(t *testing.T) {
-		category1 := entity.Category{}
-		db.Where("id = ?", 1).Delete(&category1)
-		category2 := entity.Category{}
-		db.Where("id = ?", 2).Delete(&category2)
-		res, err := categoryRepo.GetAllCategory()
-
-		assert.NotNil(t, err)
-		assert.Equal(t, false, len(res) > 0)
-	})
+			assert.NotNil(t, err)
+			assert.Equal(t, false, len(res) > 0)
+		},
+	)
 }
 
 func TestGetCategoryByID(t *testing.T) {
 	config := config.GetConfig()
 	db := util.InitDB(config)
 
-	db.AutoMigrate(&entity.Category{})
-
 	categoryRepo := NewCategoryRepository(db)
-
-	newCategory := entity.Category{}
-	newCategory.Name = "category2"
+	newCategory := entity.Category{
+		Name: "Makanan",
+	}
 
 	db.Create(&newCategory)
 
-	t.Run("1. Success get category", func(t *testing.T) {
-		res, err := categoryRepo.GetCategoryByID(2)
+	t.Run(
+		"1. Success get category", func(t *testing.T) {
 
-		assert.Nil(t, err)
-		assert.Equal(t, newCategory.Name, res.Name)
-	})
+			res, err := categoryRepo.GetCategoryByID(3)
 
-	t.Run("2. Error get category - category id not found", func(t *testing.T) {
-		res, err := categoryRepo.GetCategoryByID(1000)
+			assert.Nil(t, err)
+			assert.Equal(t, "Makanan", res.Name)
+		},
+	)
 
-		assert.NotNil(t, err)
-		assert.Equal(t, "", res.Name)
-	})
+	t.Run(
+		"2. Error get category - category id not found", func(t *testing.T) {
+			res, err := categoryRepo.GetCategoryByID(1000)
+
+			assert.NotNil(t, err)
+			assert.Equal(t, "", res.Name)
+		},
+	)
 }
 
 func TestUpdateCategory(t *testing.T) {
 	config := config.GetConfig()
 	db := util.InitDB(config)
 
-	db.AutoMigrate(&entity.Category{})
-
 	categoryRepo := NewCategoryRepository(db)
 
-	newCategory := entity.Category{}
-	newCategory.Name = "category2"
+	t.Run(
+		"1. Success update category", func(t *testing.T) {
+			mockRequest := entity.Category{
+				Model: gorm.Model{},
+				Name:  "Updated Name",
+			}
+			res, err := categoryRepo.UpdateCategory(3, mockRequest)
 
-	db.Create(&newCategory)
+			assert.Nil(t, err)
+			assert.Equal(t, "Updated Name", res.Name)
+		},
+	)
 
-	updatedCategory := entity.Category{}
-	updatedCategory.Name = "category2 new"
+	t.Run(
+		"2. Error update category - category not found", func(t *testing.T) {
+			mockRequest := entity.Category{
+				Model: gorm.Model{},
+				ID:    100,
+				Name:  "Updated Name",
+			}
+			res, err := categoryRepo.UpdateCategory(1000, mockRequest)
 
-	t.Run("1. Success update category", func(t *testing.T) {
-		res, err := categoryRepo.UpdateCategory(2, updatedCategory)
-
-		assert.Nil(t, err)
-		assert.Equal(t, updatedCategory.Name, res.Name)
-	})
-
-	t.Run("2. Error update category - category not found", func(t *testing.T) {
-		res, err := categoryRepo.UpdateCategory(1000, updatedCategory)
-
-		assert.NotNil(t, err)
-		assert.Equal(t, "", res.Name)
-	})
+			assert.NotNil(t, err)
+			assert.Equal(t, "", res.Name)
+		},
+	)
 }
 
 func TestDeleteCategory(t *testing.T) {
 	config := config.GetConfig()
 	db := util.InitDB(config)
 
-	db.AutoMigrate(&entity.Category{})
-
 	categoryRepo := NewCategoryRepository(db)
 
-	newCategory := entity.Category{}
-	newCategory.Name = "category2"
+	t.Run(
+		"1. Success Delete category", func(t *testing.T) {
+			_, err := categoryRepo.DeleteCategory(3)
 
-	db.Create(&newCategory)
+			assert.Nil(t, err)
+		},
+	)
 
-	t.Run("1. Success update category", func(t *testing.T) {
-		_, err := categoryRepo.DeleteCategory(2)
+	t.Run(
+		"2. Error update category - category not found", func(t *testing.T) {
+			_, err := categoryRepo.DeleteCategory(100)
 
-		assert.Nil(t, err)
-	})
-
-	t.Run("2. Error update category - category not found", func(t *testing.T) {
-		_, err := categoryRepo.DeleteCategory(100)
-
-		assert.NotNil(t, err)
-	})
+			assert.NotNil(t, err)
+		},
+	)
 }
